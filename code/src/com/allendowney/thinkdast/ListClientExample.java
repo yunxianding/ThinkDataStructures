28db04cf7cd8e84302d9956263372cfbbfcd5df4
yunxiann <yding41@smith.edu>
2025-05-17 21:55:06 -0400

    Finished exercise 1
    Simply changing LinkedList to ArrayList, remembered to import ArrayList

--- code/src/com/allendowney/thinkdast/ListClientExample.java
+++ code/src/com/allendowney/thinkdast/ListClientExample.java
@@ -1,7 +1,7 @@
 package com.allendowney.thinkdast;
 
-import java.util.LinkedList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ListClientExample {
 	@SuppressWarnings("rawtypes")
@@ -9,7 +9,7 @@ public class ListClientExample {
 
 	@SuppressWarnings("rawtypes")
 	public ListClientExample() {
-		list = new LinkedList();
+		list = new ArrayList();
 	}
 
 	@SuppressWarnings("rawtypes")
